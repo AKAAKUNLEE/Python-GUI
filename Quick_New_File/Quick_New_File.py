a32742a625736083759e52d2f3942019ae94e343
liankun <644469786@qq.com>
2024-10-03 11:38:34 +0800

    first code

---
+++ Quick_New_File/Quick_New_File.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+"""
+@Project ：url快速转换快捷方式.py 
+@File    ：Quick_New_File.py
+@IDE     ：PyCharm 
+@Author  ：AKUNLEE
+@Date    ：2024/10/3 11:34 
+"""
+import tkinter as tk # 使用Tkinter前需要先导入
+import tkinter.messagebox
+import pickle
---
+++ Quick_New_File/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+"""
+@Project ：url快速转换快捷方式.py 
+@File    ：__init__.py.py
+@IDE     ：PyCharm 
+@Author  ：AKUNLEE
+@Date    ：2024/10/3 11:33 
+"""
